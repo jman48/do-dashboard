f4d70710fb814f9669d0a7e4b2ed6c627a360625
John Armstrong <j.armstrong484@gmail.com>
2018-11-12 16:45:34 +0800

    Add ads to app

---
+++ src/modules/core/components/BannerAd.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import firebase from 'react-native-firebase';
+
+const Banner = firebase.admob.Banner;
+const AdRequest = firebase.admob.AdRequest;
+const request = new AdRequest();
+
+const BannerAd = () => (
+  <Banner
+    unitId={'ca-app-pub-1196600699219140/2972943317'}
+    size={"SMART_BANNER"}
+    request={request.build()}
+  />
+);
+
+export default BannerAd;
---
+++ src/modules/core/utils/ad.js
@@ -0,0 +1,3 @@
+import firebase from 'react-native-firebase';
+
+firebase.admob().initialize('ca-app-pub-1196600699219140~5870569392');
